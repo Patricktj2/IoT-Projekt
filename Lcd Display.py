60e84b529dc436315526422d870feb3ef2d524b2
patricktj <patricktalmer@gmail.com>
2025-12-08 10:48:51 +0100

    v1.1 gps added

--- Lcd Display.py
+++ Lcd Display.py
@@ -1,13 +1,25 @@
-from machine import Pin
+from machine import Pin, UART
 from uthingsboard.client import TBDeviceMqttClient
 from gpio_lcd import GpioLcd
 from time import sleep, ticks_ms
 from lmt87 import LMT87
+from gps_simple import GPS_SIMPLE
 import random
 import secrets
 
 pin_lmt87 = 35
 temp = LMT87(pin_lmt87)
+
+# Gps ting ting
+gpsPort = 2                                 # ESP32 UART port, Educaboard ESP32 default UART port
+gpsSpeed = 9600                             # UART speed, defauls u-blox speed
+gpsEcho = False                             # Echo NMEA frames: True or False
+gpsAllNMEA = False                          # Enable all NMEA frames: True or False
+uart = UART(gpsPort, gpsSpeed)              # UART object creation
+gps = GPS_SIMPLE(uart, gpsAllNMEA)          # GPS object creation
+threaded = False
+pin_lmt87 = 35
+average = 1
 
 # Laver lcd object
 lcd = GpioLcd(rs_pin=Pin(27), enable_pin=Pin(25), d4_pin=Pin(33),
@@ -33,6 +45,7 @@ class timer:
 # Non blocking timers til de forskellige functions, skrives sådan: "selv lavet timer".non_blocking_timer("function man skal køre")
 display_test_timer = timer(200)
 temp_display_timer = timer(500)
+gps_module_timer = timer(1000)
 
 # Forbinder til thingsboard
 client = TBDeviceMqttClient(secrets.SERVER_IP_ADDRESS, access_token = secrets.ACCESS_TOKEN)
@@ -41,7 +54,7 @@ print("Forbundet til thingsboard")
 
 # Display test function
 def display_test():
-    numbers = [0,1,2,3,4,5,6,7,8,]
+    numbers = [0,1,2,3,4,5,6,7,8,9]
     lcd.move_to(19,3)
     lcd.putstr(str(random.choice(numbers)))
  
@@ -49,6 +62,13 @@ def display_test():
 def speed_display():
     lcd.move_to(0,0)
     lcd.putstr("Test")
+
+def gps_module():
+    if (gps.receive_nmea_data(gpsEcho)):
+        print("Speed         : %.1f m/s" % gps.get_speed())
+        print("Latitude      : %.8f" % gps.get_latitude())
+        print("Longitude     : %.8f" % gps.get_longitude())
+        print("Course        : %.1f°" % gps.get_course())
 
 #Program
 print("LMT87 test\n")
@@ -85,6 +105,7 @@ try:
     while True:
         temp_display_timer.non_blocking_timer(temp_display)
         display_test_timer.non_blocking_timer(display_test)
+        gps_module_timer.non_blocking_timer(gps_module)
 
 except KeyboardInterrupt:
     print("No more banana :(")

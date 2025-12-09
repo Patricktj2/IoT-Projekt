ef74b00d0ddb4fca994e377dde06764e30d01e2f
patricktj <patricktalmer@gmail.com>
2025-12-09 11:05:57 +0100

    Main educarboard fil Display info + tyverialarm

---
+++ main.py
@@ -0,0 +1,217 @@
+from uthingsboard.client import TBDeviceMqttClient
+from time import sleep, ticks_ms
+from machine import reset, UART, Pin, PWM
+from gps_simple import GPS_SIMPLE
+from math import sin, cos, sqrt, atan2, radians
+from gpio_lcd import GpioLcd
+from lmt87 import LMT87
+import gc
+import secrets
+import random
+
+ref_lat = None
+ref_lon = None
+
+pin_lmt87 = 35
+average = 1
+alarm_enabled = False
+temp = LMT87(pin_lmt87)
+
+# Gps/buzzer/led stuff
+gpsPort = 2                                 # ESP32 UART port, Educaboard ESP32 default UART port
+gpsSpeed = 9600                             # UART speed, defauls u-blox speed
+gpsEcho = False                             # Echo NMEA frames: True or False
+gpsAllNMEA = False                          # Enable all NMEA frames: True or False
+uart = UART(gpsPort, gpsSpeed)              # UART object creation
+gps = GPS_SIMPLE(uart, gpsAllNMEA)          # GPS object creation
+led = Pin(12, Pin.OUT)
+buzz = PWM(Pin(21))
+buzz.freq(1000)       # loud frequency
+buzz.duty_u16(0)      # start silent
+threaded = False                            # Use threaded (True) or loop (False)
+
+# Laver lcd object
+lcd = GpioLcd(rs_pin=Pin(27), enable_pin=Pin(25), d4_pin=Pin(33),
+               d5_pin=Pin(32), d6_pin=Pin(21), d7_pin=Pin(22), num_lines=4,
+              num_columns=20)
+
+# Tænder og clearer displayet når koden starter
+lcd.backlight_on()
+lcd.display_on()
+lcd.clear()
+
+# Forbinder til thingsboard
+client = TBDeviceMqttClient(secrets.SERVER_IP_ADDRESS, access_token = secrets.ACCESS_TOKEN)
+client.connect()
+print("Forbundet til thingsboard")
+
+# En class til en non blocking timer
+class timer:
+    def __init__(self, delay_period_ms):
+        self.start_time = ticks_ms()
+        self.delay_period_ms = delay_period_ms
+    
+    def non_blocking_timer(self, func):
+        if ticks_ms() - self.start_time > self.delay_period_ms:
+            func() # Starter funktionen når tiden er gået
+            self.start_time = ticks_ms() # Genstarter tiden
+
+# Non blocking timers til de forskellige functions, skrives sådan: "selv lavet timer".non_blocking_timer("function man skal køre")
+display_test_timer = timer(200)
+temp_display_timer = timer(500)
+gps_module_timer = timer(1000)
+
+# Thingsboard functions
+def rpc_request(req_id, method, params):
+    """handler callback to recieve RPC from server """
+    global alarm_enabled
+    print(f'Response {req_id}: {method}, params {params}')
+    print(params, "params type:", type(params))
+    try:
+        # check if the method is "toggle_led1" (needs to be configured on thingsboard dashboard)
+        if method == "alarmtrigger":
+            # check if the value is is "led1 on"
+            alarm_enabled = params
+    except Exception as e:
+        print("RPC handler error:", e)
+        
+## Display functions##
+
+# Hastighed display function
+def speed_display():
+    if (gps.recieve_nmea_data(gpsEcho)):
+        lcd.move_to(0,0)
+        lcd.putstr(gps.get_speed())
+
+# Display test function
+def display_test():
+    numbers = [0,1,2,3,4,5,6,7,8,9]
+    lcd.move_to(19,3)
+    lcd.putstr(str(random.choice(numbers)))
+
+# Gps modul function med speed, lat, long og course
+def gps_module():
+    if (gps.receive_nmea_data(gpsEcho)):
+        lcd.move_to(0, 0)
+        lcd.putstr("%d m/s" % gps.get_speed())
+        
+        
+        
+        client.send_telemetry({
+        "speed": gps.get_speed(),
+        "latitude": gps.get_latitude(),
+        "longitude": gps.get_longitude(),
+        "course": gps.get_course()
+        })
+        
+# Temperatur display function
+def temp_display():
+    custom_chr = bytearray([0b00010,
+                            0b00101,
+                            0b00010,
+                            0b00000,
+                            0b00000,
+                            0b00000,
+                            0b00000,
+                            0b00000])
+
+    adc_val = temp.get_adc_value()
+    temperature = temp.get_temperature()
+    
+    client.send_telemetry({"temperature": temperature})
+    
+    lcd.move_to(16, 1)
+    lcd.putstr("%d C" % (temperature))
+    lcd.move_to(18, 1)
+    lcd.custom_char(0, custom_chr)
+    lcd.putchar(chr(0))
+
+## Tyverialarm functions ##
+
+# Selve alarm funktionen
+def alarm():
+    while alarm_enabled:
+        led.value(1)
+        buzz.duty_u16(40000)   # loud volume (max ~65535)
+        sleep(1)
+
+        led.value(0)
+        buzz.duty_u16(0)       # off
+        sleep(0.5)
+        client.check_msg()
+        client.set_server_side_rpc_request_handler(rpc_request)
+
+# Udregner distance 
+def distance_m(lat1, lon1, lat2, lon2):
+    
+    R = 6371000 
+    phi1 = radians(lat1)
+    phi2 = radians(lat2)
+    dphi = radians(lat2 - lat1)
+    dlambda = radians(lon2 - lon1)
+
+    a = sin(dphi/2)**2 + cos(phi1) * cos(phi2) * sin(dlambda/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return R * c
+    #Bliver brugt til at udregne distancen mellem to punkter (lon og lat)
+
+# Funktion som trigger alarmen
+def alarmtrigger_step():
+    global ref_lat, ref_lon
+    if (gps.receive_nmea_data(gpsEcho)):
+        gps.get_latitude()
+        gps.get_longitude()
+    
+    Location = {
+        "Latitude": gps.get_latitude(),
+        "Longitude": gps.get_longitude()
+    }
+    #Starter ikke programmet hvis ingen lokation er fundet
+    if Location["Latitude"] == -999 or Location["Longitude"] == -999:
+        print("Uh oh no sattelite found oopsie woopsie")
+        sleep(0.5)
+        return False
+
+    # Set reference position once (on first valid fix)
+    if ref_lat is None:
+        ref_lat = Location["Latitude"]
+        ref_lon = Location["Longitude"]
+        print("Reference set to:", ref_lat, ref_lon)
+        sleep(0.5)
+        return False
+
+    dist = distance_m(ref_lat, ref_lon,
+                      Location["Latitude"], Location["Longitude"])
+
+    print("Current:", Location.values(), "Distance (m):", dist)
+    sleep(0.5)
+
+    # Her kan der indstilles hvor langt cyklen må flytte sig i meter
+    if dist > 1:
+        alarm()
+        
+    return False
+
+# Intiater functions og sender data til thingsboard
+try:
+    while True:
+        temp_display_timer.non_blocking_timer(temp_display)
+        display_test_timer.non_blocking_timer(display_test)
+        gps_module_timer.non_blocking_timer(gps_module)
+        
+        client.check_msg()
+        client.set_server_side_rpc_request_handler(rpc_request)
+        
+        if alarm_enabled:
+            triggered = alarmtrigger_step()
+            if triggered:
+                print("ALARM TRIGGERED") 
+            alarmtrigger_step()
+            continue
+            
+except KeyboardInterrupt:
+    print("No more banana :(")
+    
+finally:
+    lcd.display_off()
+    lcd.backlight_off()
